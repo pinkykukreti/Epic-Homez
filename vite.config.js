1ef4980519ce2f644ba306d2be640a5a25492536
pinkykukreti <pinky.kukreti@gmail.com>
2026-09-23 17:50:10 +0530

    change vite file config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
+// import { defineConfig } from "vite";
+// export default defineConfig({
+//   esbuild: { jsx: "automatic" },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           react: ["react", "react-dom", "react-router-dom"],
+//           supabase: ["@supabase/supabase-js"],
+//         },
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
+
 export default defineConfig({
+  base: "/Epic-Homez/",
   esbuild: { jsx: "automatic" },
   build: {
     rollupOptions: {
